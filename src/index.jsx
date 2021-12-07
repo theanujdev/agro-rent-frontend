583234b07ad0e8063e7f93725bb01c0f5a3530a7
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-12-07 19:28:47 +0530

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
